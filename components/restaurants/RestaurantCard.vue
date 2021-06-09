d34c6e0b60227528ab8a9a20b0db628625e4c4a7
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-06-09 16:26:13 +0900

    [update] お気に入り(favo)機能の追加

--- components/restaurants/RestaurantCard.vue
+++ components/restaurants/RestaurantCard.vue
@@ -4,6 +4,12 @@
     hover
     rounded
   >
+    <v-btn icon class="icon" v-if="!nowFavo" @click="setStorage(true)">
+      <v-icon large>mdi-star</v-icon>
+    </v-btn>
+    <v-btn icon class="icon" v-if="nowFavo" color="#FBC02D" @click="setStorage(false)">
+      <v-icon large>mdi-star</v-icon>
+    </v-btn>
     <v-img height="500px" :src="item.photo.pc.l" />
     <h2 class="text">{{ item.name }}</h2>
     <p v-if="item.catch" class="text">{{ item.catch }}</p>
@@ -12,8 +18,49 @@
 
 <script>
 export default {
+  data () {
+    return {
+      nowFavo: false
+    }
+  },
   props: {
     item: Object
+  },
+  async created () {
+    this.nowFavo = this.favo()
+  },
+  methods: {
+    favo () {
+      const list = this.getLocalStrage() 
+      if (list.length === 0) { return false }
+
+      const targetId = this.item.id
+      const include = list.some(function(v, i) {
+        if (v.id === targetId) return true
+      })
+      return include
+    },
+    getLocalStrage() {
+      const list = JSON.parse(localStorage.getItem('favolist'))
+      if (list === null) {
+        localStorage.setItem("favolist", JSON.stringify([]))
+        return []
+      }
+      return list
+    },
+    setStorage(boolen) {
+      let list = this.getLocalStrage()
+      if (boolen) {
+        list.push(this.item)
+      } else {
+        const targetId = this.item.id
+        list.some(function(v, i) {
+          if (v.id == targetId) list.splice(i,1);
+        })
+      }
+      window.localStorage.setItem("favolist", JSON.stringify(list));
+      this.nowFavo = this.favo()
+    }
   }
 }
 </script>
@@ -21,6 +68,13 @@ export default {
 <style scoped>
 .card {
   text-align: left;
+  position: relative;
+}
+.icon {
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  z-index: 7;
 }
 .text {
   width: 95%;
