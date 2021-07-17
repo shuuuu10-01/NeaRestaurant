663919cad4a2960c61a06ce23256cde2f91f153b
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-07-17 15:08:39 +0900

    [change] footerの表示名を変更

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-m.gif" alt="ホットペッパー Webサービス" width="88" height="35" border="0" title="ホットペッパー Webサービス"></a>
-    <p>© {{ getYear() }} ShuTanaka </p>
+    <p>© {{ getYear() }} shuuuu </p>
   </footer>
 </template>
 
